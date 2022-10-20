ebbcf252f5bdcc1dc5aa93fe2d3518c66f04dbf9
BGMuirhead <81272216+BGMuirhead@users.noreply.github.com>
2022-10-21 09:04:01 +1300

    Updated for main_driver

--- ElGamal.py
+++ ElGamal.py
@@ -1,11 +1,8 @@
 
 
-global base # base of algorithm
-global prime #prime for algorithm 
-global discreteLogs # used to store the 'table' of logs
 
 #function calculates the table of logs based on the base and the prime
-def discreteLogFunc():
+def discreteLogFunc(prime,base):
     global discreteLogs
     discreteLogs = [] #list to store values
     
@@ -15,63 +12,75 @@ def discreteLogFunc():
 
     return discreteLogs
 
-#function encodes an input message with input secret k and public key of reciever
+#function encodes an input message with input secret k and public key of receiver
 def encodeElGamal():
-    global base
-    global prime
-    message = int(input("What is the Message: ")) #get the message 
-    secretk =  int(input("What is the secret k: ")) # get the secret k 
-    recieverKey =  int(input("What is the reciever's Public Key: ")) # get the public key of the reciever
-        
-    K = (recieverKey**secretk) % prime # determine K
-    ak = (base**secretk) %prime # determine aK
-    Km = (K*message) %prime # determine Km
-    
-    #output the values, for debugging
-    print("K: "+str(K))
-    print("ak: "+str(ak))
-    print("Km: "+str(Km))
+    while(True):
+        try:
+            prime = int(input("What is the Prime Number: ")) # get the prime for the system
+            base = int(input("What is the Base Number: ")) # get the base for the system
 
-    #output the pair of values for the cipher text
-    print("Cipher Text: " + str(ak) +" "+ str(Km))
+            discreteLogs = discreteLogFunc(prime,base) # populate the log table
+            
+            message = int(input("What is the Message: ")) #get the message 
+            secretk =  int(input("What is the secret k: ")) # get the secret k 
+            receiverKey =  int(input("What is the receiver's Public Key: ")) # get the public key of the receiver
+                
+            K = (receiverKey**secretk) % prime # determine K
+            ak = (base**secretk) %prime # determine aK
+            Km = (K*message) %prime # determine Km
+            
+            #output the values, for debugging
+            print("K: "+str(K))
+            print("ak: "+str(ak))
+            print("Km: "+str(Km))
 
+            #output the pair of values for the cipher text
+            print("Cipher Text: " + str(ak) +", "+ str(Km))
+            return
+        except:
+            print("Invalid Input Please Try Again")
 
 
 #function decodes an encoded message
 def decodeElGamal():
-    global base
-    global prime
-    global discreteLogs
-    ak = int(input("What is the first part of the cipher text : ")) #get the first part of the cipher text
-    Km =  int(input("What is the second part of the cipher text : ")) # get the second part of the cipher text
-    recieverKey = int(input("What is the reciever's public key : "))
-    
-    #determine the private number
-    privateNum =0
+    while(True):
+        try:
+            prime = int(input("What is the Prime Number: ")) # get the prime for the system
+            base = int(input("What is the Base Number: ")) # get the base for the system
+            discreteLogs = discreteLogFunc(prime,base) # populate the log table   
 
-    #for each value in the table of logs
-    for i in range(len(discreteLogs)):
-        if discreteLogs[i]==recieverKey: # get the location of the value that matches ak 
-            privateNum=i+1 # index value is the private value of the reciever
-            break
+            ak = int(input("What is the first part of the cipher text : ")) #get the first part of the cipher text
+            Km =  int(input("What is the second part of the cipher text : ")) # get the second part of the cipher text
+            receiverKey = int(input("What is the receiver's public key : "))
+            
+            #determine the private number
+            privateNum =0
 
-    print("Reciever Private k:" + str(privateNum))
-    
-    K = (ak **privateNum) % prime
+            #for each value in the table of logs
+            for i in range(len(discreteLogs)):
+                if discreteLogs[i]==receiverKey: # get the location of the value that matches ak 
+                    privateNum=i+1 # index value is the private value of the receiver
+                    break
 
-    print("K: "+ str(K))  #print the value of K, for debugging
-    inverseK = pow(K, prime-2, prime) # calculate the inverse of K modulo the prime
-    
-    print("K^-1: "+str(inverseK)) #print the value of K inverse, for debugging
-   
-    message = (inverseK * Km) % prime  # use K inverse to decrypt the message
-   
-    print("Original Message is: "+str(message))  #print the decrypted message
+            print("receiver Private k:" + str(privateNum))
+            
+            K = (ak **privateNum) % prime
+
+            print("K: "+ str(K))  #print the value of K, for debugging
+            inverseK = pow(K, prime-2, prime) # calculate the inverse of K modulo the prime
+            
+            print("K^-1: "+str(inverseK)) #print the value of K inverse, for debugging
+        
+            message = (inverseK * Km) % prime  # use K inverse to decrypt the message
+        
+            print("Original Message is: "+str(message))  #print the decrypted message
+            return
+        except:
+            print("Invalid Input Please Try Again")
 
 
 
-
-
+"""
 prime = int(input("What is the Prime Number: ")) # get the prime for the system
 base = int(input("What is the Base Number: ")) # get the base for the system
 
@@ -93,6 +102,7 @@ while True:
     else:
         #invalid - get input again
         choice = input("Please enter E/e for encryption or enter D/d for decryption: ")
+"""
 
 
 
